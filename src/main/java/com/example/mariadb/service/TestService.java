f9ca53f25aa0680938b8208f63ab88dadfa49921
pedrowillliam <pedrowilliam014@hotmail.com>
2024-09-15 18:37:50 -0300

    adicionando mais algumas consultas.

--- src/main/java/com/example/mariadb/service/TestService.java
+++ src/main/java/com/example/mariadb/service/TestService.java
@@ -1,5 +1,8 @@
 package com.example.mariadb.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
@@ -15,4 +18,44 @@ public class TestService {
         Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
         System.out.println("Número de produtos: " + count);
     }
+
+     public List<Map<String, Object>> getAllProducts() {
+        String sql = "SELECT * FROM produto";
+        return jdbcTemplate.queryForList(sql);
+    }
+
+     // 1. Consulta para retornar produtos de fornecedores do Japão
+     public List<Map<String, Object>> getProductsFromJapan() {
+        String sql = """
+            SELECT p.nome, pi.descricao, p.preco_venda_minimo
+            FROM Produto p
+            JOIN ProdutoIdioma pi ON p.produto_id = pi.produto_id
+            JOIN Fornecedor f ON p.fornecedor_id = f.fornecedor_id
+            JOIN Estoque e ON p.produto_id = e.produto_id
+            WHERE f.pais = 'Japão'
+            GROUP BY p.produto_id, p.nome, pi.descricao, p.preco_venda_minimo
+            HAVING MIN(e.quantidade) > 120
+        """;
+        
+        return jdbcTemplate.queryForList(sql);
+     }
+
+      // 4. Método para retornar dados de armazéns e estoques com soma de quantidade >= 200
+     public List<Map<String, Object>> getWarehouseStockSummary() {
+        String sql = """
+            SELECT a.nome AS NomeArmazem, 
+                   e.codigo AS CodigoEstoque, 
+                   c.nome AS NomeCategoria, 
+                   SUM(e.quantidade) AS TotalQuantidade, 
+                   AVG(e.quantidade) AS MediaQuantidade
+            FROM Estoque e
+            JOIN Armazem a ON e.armazem_id = a.armazem_id
+            JOIN Produto p ON e.produto_id = p.produto_id
+            JOIN Categoria c ON p.categoria_id = c.categoria_id
+            GROUP BY a.nome, e.codigo, c.nome
+            HAVING SUM(e.quantidade) >= 200
+        """;
+
+        return jdbcTemplate.queryForList(sql);
+     }
 }
